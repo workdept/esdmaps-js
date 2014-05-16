f306478009d59c46dd80cdc5e7bfc31606e858ba
Geoff Hing <geoffhing@gmail.com>
2014-05-16 15:20:50 -0500

    Clean up dependency bootstrap script, add example.

--- esdmaps-embed.js
+++ esdmaps-embed.js
@@ -1,20 +1,22 @@
 (function(window) {
-  MAPBOX_JS = '//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.js';
-  MAPBOX_CSS = '//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.css';
-  ESDMAPS_JS = '';
+  var MAPBOX_JS = '//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.js';
+  var MAPBOX_CSS = '//api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.css';
+  var ESDMAPS_JS = thisURL().replace('esdmaps-embed', 'esdmaps');
 
   function getRemote(url, el, attr, callback) {
     var done = false;
     var proto = window.location.protocol === 'file:' ? 'http:' : '';
     el[attr] = proto + url;
     // Attach handlers for all browsers
-    el.onload = script.onreadystatechange = function() {
+    el.onload = el.onreadystatechange = function() {
       if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
         done = true;
         
-        callback();
+        if (callback) {
+          callback();
+        }
         
-        el.onload = script.onreadystatechange = null;
+        el.onload = el.onreadystatechange = null;
       }
     };
 
@@ -22,7 +24,7 @@
   }
 
   function getScript(url, callback) {
-    var script = root.document.createElement('script'); 
+    var script = document.createElement('script'); 
     getRemote(url, script, 'src', callback);
   }
 
@@ -39,18 +41,41 @@
 
   function getStylesheet(url, callback) {
     var el = document.createElement('link');
+    el.rel = 'stylesheet';
     getRemote(url, el, 'href', callback);
   }
 
-  if (typeof L == 'undefined' || typeof L.mapbox == 'undefined') {
-    getScript(MAPBOX_JS);
+  function getESDMapsScript() {
+    if (typeof ESDMaps == 'undefined') {
+      getScript(ESDMAPS_JS);
+    }
+  }
+
+  /**
+   * Get the url of this script 
+   */
+  function thisURL() {
+    var scripts = document.getElementsByTagName('script');
+    
+    for (var i = 0; i < scripts.length; i++) {
+      s = scripts[i];
+
+      if (s.src && s.src.match(/esdmaps-embed\.js$/)) {
+        return s.src;
+      }
+    }
+
+    return null;
   }
 
   if (!hasStylesheet(MAPBOX_CSS)) {
     getStylesheet(MAPBOX_CSS);
   }
 
-  if (typeof ESDMaps == 'undefined') {
-    getScript(ESDMAPS_JS);
+  if (typeof L == 'undefined' || typeof L.mapbox == 'undefined') {
+    getScript(MAPBOX_JS, getESDMapsScript); 
+  }
+  else {
+    getESDMapsScript();
   }
 })(window, document);
